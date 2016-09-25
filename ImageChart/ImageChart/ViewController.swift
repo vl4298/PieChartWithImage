97ca9fc220babd8c7cbd3127a65f9a96484ae002
Van Luu <valu4298@gmail.com>
2016-09-25 12:08:06 +0700

    update: add gesture

--- ImageChart/ImageChart/ViewController.swift
+++ ImageChart/ImageChart/ViewController.swift
@@ -36,49 +36,64 @@ class ViewController: UIViewController {
   
   var chartView: PieChartView!
   var startAngle: CGFloat = 0
-  var displayLink: CADisplayLink!
-  var radianPerSecond: Double = 5
+  var displayLink: CADisplayLink?
+  var radianPerSecond: Double = 0
   var lastTimeTick: TimeInterval! = 0
   
   override func viewDidAppear(_ animated: Bool) {
     super.viewDidAppear(animated)
     chartView = PieChartView(frame: CGRect(x: 0, y: 0, width: 400, height: 400))
     view.addSubview(chartView)
+
+    let dragGesture = UIPanGestureRecognizer(target: self, action: #selector(runGesture(gesture:)))
+    chartView.addGestureRecognizer(dragGesture)
   }
   
   
   
-  func draw2() -> UIView {
-    let view = UIView(frame: CGRect(x: 0, y: 0, width: 400, height: 400))
-    view.backgroundColor = UIColor.red
-    return view
+  func runGesture(gesture: UIPanGestureRecognizer) {
+    
+    switch gesture.state {
+    case .began:
+      if displayLink == nil {
+        displayLink = CADisplayLink(target: self, selector: #selector(updateChartView))
+        displayLink!.preferredFramesPerSecond = 60
+        displayLink!.add(to: RunLoop.current, forMode: RunLoopMode.commonModes)
+      }
+    case .cancelled, .ended:
+      if displayLink != nil {
+        displayLink?.invalidate()
+        displayLink = nil
+      }
+    default:
+      radianPerSecond = Double(gesture.velocity(in: self.chartView!).y)/2
+
+    }
   }
   
   var finalRadian: CGFloat = 0
   @IBAction func rotate(sender: UIButton) {
-    finalRadian = chartView.startAngle + 100
-    displayLink = CADisplayLink(target: self, selector: #selector(updateChartView))
-    displayLink.preferredFramesPerSecond = 1
-    displayLink.add(to: RunLoop.current, forMode: RunLoopMode.commonModes)
+//    finalRadian = chartView.startAngle + 100
+    
+    
+    let rotateAnimation = CABasicAnimation(keyPath: "transform.rotation")
+    rotateAnimation.fromValue = 0
+    rotateAnimation.toValue = M_PI
+    rotateAnimation.duration = 3.0
+    
+    chartView.layer.add(rotateAnimation, forKey: "rotate")
   }
   
   func updateChartView() {
+    print("run")
     if lastTimeTick == 0 {
       lastTimeTick = displayLink!.timestamp
     }
     
     let currentTimestamp = displayLink!.timestamp
     let delta = CGFloat(radianPerSecond * (currentTimestamp - lastTimeTick))
-    print(delta)
     chartView.startAngle += delta
     lastTimeTick = currentTimestamp
-    
-    if chartView.startAngle >= finalRadian {
-      chartView.startAngle = finalRadian
-      displayLink.invalidate()
-      displayLink = nil
-    }
-    
     
   }
 
